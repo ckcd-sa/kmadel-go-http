ce4cce29b258ddf83eabc8e4a575f7295c1f4a17
Kurt Madel <kmadel@mac.com>
2019-02-24 19:57:13 +0000

    Updated title

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example with changes by kmadel"
 
 	from := ""
 	if r.URL != nil {
